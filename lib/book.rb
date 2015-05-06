040bedecf3b26f64f2b97b27eec6f9dcf651614a
Jeanette Fairless & Steve Schneider <student@epicodus.com>
2015-05-06 14:18:16 -0700

    add spec for equal, update, delete. add method code to pass all specs.

--- lib/book.rb
+++ lib/book.rb
@@ -19,6 +19,31 @@ attr_reader(:title, :author, :id)
     books
   end
 
+  define_singleton_method(:find) do |id|
+    @id = id
+    result = DB.exec("SELECT * FROM books WHERE id = #{@id};")
+    @title = result.first().fetch('title')
+    @author = result.first().fetch('author')
+    Book.new({:title => @title, :author => @author, :id => @id})
+  end
 
+  define_method(:save) do
+    result = DB.exec("INSERT INTO books (title, author) VALUES ('#{@title}', '#{@author}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
+
+  define_method(:==) do |another_title|
+    self.title().==(another_title.title()).&(self.id().==(another_title.id())).&(self.author().==(another_title.author()))
+  end
+
+  define_method(:update) do |attributes|
+    @title = attributes.fetch(:title, @title)
+    @id = self.id()
+    DB.exec("UPDATE books SET title = '#{@title}' WHERE id = #{@id};")
+  end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM books WHERE id = #{self.id()};")
+  end
 
 end
